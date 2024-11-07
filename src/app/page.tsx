eb126b0cda57235273d8de60764daaf630c63fa5
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-11-07 22:52:32 +0530

    page.tsx updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { TodoCard } from "@/components/todoCard";
 
 export default function Home() {
   return (
-    <main className="relative max-w-full w-full h-screen">
+    <main className="relative max-w-full w-full h-svh">
         <TodoCard />
     </main>
   );
