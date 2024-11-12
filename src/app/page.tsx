925ba10fc091d57c466d2a25ebc102766f98b064
saurabh-dev-vns <saurabhsahani0001@gmail.com>
2024-11-12 22:58:35 +0530

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { TodoCard } from "../components/TodoCard";
-import { Toaster } from "react-hot-toast";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <main className="relative max-w-full w-full h-svh">
-      <TodoCard />
-      <Toaster position="top-center" reverseOrder={false} />
+    <main className="relative flex m-3 sm:m-1 max-w-full w-full justify-center items-center h-svh overflow-hidden">
+        <div className="flex justify-center items-center flex-col w-full h-svh sm:max-w-3xl overflow-hidden">
+            <h2>TODO</h2>
+            <p className={`text-center`}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto dicta quis deleniti odit vero dolorum earum dolores debitis vel architecto. Porro voluptatum iure omnis reprehenderit, corporis quis dolores nam! Tempore.
+                </p>
+            <button>Get Started</button>
+        </div>
     </main>
   );
 }
